cfc9f0dbe097f7d280b8d2a7393e0f272997f8bb
Andrey <apalchev@gmail.com>
2023-03-09 16:06:42 +0300

    practise file

---
+++ Practise/Oop.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Animal {
+protected:
+    string name;
+    string classis;
+    string sound;
+    string food;
+    string color;
+    /*
+    0-very small
+    ...
+    10 - very big
+    */
+    unsigned short size;
+public:
+    Animal(string name, string classis, string sound, string food, string color, unsigned short size) {
+        this->classis = classis;
+        this->color = color;
+        this->food = food;
+        this->name = name;
+        this->size = size;
+        this->sound = sound;
+    }
+    void breathe() {
+        cout << "inhale" << "  " << " exhale" << endl;;
+    }
+    void meal() {
+        cout << "ate "<<this->food << endl;;
+    }
+    void makeASound()
+    {
+        cout << "says: " << this->sound << endl;;
+    }
+    ~Animal() {
+        cout << this->name << " was killed" << endl;;
+    }
+};
+
+class Elephant : public Animal {
+private:
+    unsigned short tusksSize;
+    const bool wool = false;
+public: 
+    Elephant(string name, string classis, string sound, string food, string color, unsigned short size, unsigned short tusksSize) :Animal(name, classis, sound, food, color, size)
+    {
+        this->tusksSize = tusksSize;
+    }
+    void breathe() {
+        cout << this->name << "make it one more time" << endl;;
+    }
+    void waterInTrunk()
+    {
+        cout << "набрал воды в хобот" << endl;
+    }
+    ~Elephant() {
+        cout << "was killed" << endl;
+    }
+};
+
+class Cow : public Animal {
+private:
+    unsigned short hornsSize;
+    unsigned short milkBucketCount;
+public:
+    Cow(string name, string classis, string sound, string food, string color, unsigned short size, unsigned short hornsSize, unsigned short milkBucketCount) :Animal(name, classis, sound, food, color, size)
+    {
+        this->hornsSize = hornsSize;
+        this->milkBucketCount = milkBucketCount;
+    }
+    void breathe() {
+        cout << this->name <<"make it two more time" << endl;;
+    }
+    void milk()
+    {
+        cout << "give " << this->milkBucketCount << "at noon" << endl;
+    }
+    ~Cow() {
+        cout << "was killed" << endl;;
+    }
+};
+
+int main()
+{
+    Cow *cow = new Cow("Burenka", "Mamal", "mooo", "grass", "multi", 4, 6, 2);
+    Elephant* eleph = new Elephant("Djambo", "Mamaml", "BLOWING", "Grass and foliage", "grey", 9, 10);
+    cow->breathe();
+    cow->milk();
+    cow->meal();
+    eleph->makeASound();
+    cow->makeASound();
+    eleph->breathe();
+    eleph->waterInTrunk();
+    eleph->meal();
+    delete eleph;
+    delete cow;
+}    
